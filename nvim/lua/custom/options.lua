12ec1994ebbee6b427249b6969ffa641a1a9f1eb
Wade Green <thewadegreen@gmail.com>
2025-10-13 01:02:04 -0400

    Add custom formatting options

---
+++ nvim/lua/custom/options.lua
@@ -0,0 +1,7 @@
+-- enforce 2-space indentation globally
+vim.opt.tabstop = 2 -- display width of a tab character
+vim.opt.shiftwidth = 2 -- number of spaces for each indent level
+vim.opt.softtabstop = 2 -- number of spaces a <Tab> inserts/deletes in insert mode
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.smartindent = true -- smart autoindenting on new lines
+vim.opt.breakindent = true -- maintain indent when wrapping lines
